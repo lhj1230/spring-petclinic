4e2bea1373f9e2dd4bc2232aa47ecd11da9e8422
lhj1230 <55931023+lhj1230@users.noreply.github.com>
2025-05-28 11:07:11 +0900

    Update start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-export PATH=$PATH:/usr/local/lib/docker/cli-plugins
+
 cd /home/ubuntu/scripts
-docker compose up -d --build
+/home/ubuntu/.docker/cli-plugins/docker-compose up -d --build
