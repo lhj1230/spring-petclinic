f3a8c12e5691ac39b940f6dafd50cc7c4e722b37
lhj1230 <55931023+lhj1230@users.noreply.github.com>
2025-05-29 13:29:45 +0900

    Update start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,20 +1,4 @@
 #!/bin/bash
 
-# Stop and remove if running
-docker stop spring-petclinic || true
-docker rm spring-petclinic || true
-
-# 강제로 포트 점유한 컨테이너도 제거
-CONTAINER_ID=$(docker ps -q --filter "publish=80")
-if [ -n "$CONTAINER_ID" ]; then
-  docker stop "$CONTAINER_ID"
-  docker rm "$CONTAINER_ID"
-fi
-
-# Run with mounted latest jar
-docker run -d -p 80:8080 \
-  --name spring-petclinic \
-  -v /home/ubuntu/target:/app \
-  openjdk:21 \
-  java -jar /app/spring-petclinic-*.jar
-
+cd /home/ubuntu/scripts
+docker compose up -d --build
