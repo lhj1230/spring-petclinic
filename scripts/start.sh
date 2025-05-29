39f7826f1cb840e2339438943b81b39239b2c216
lhj1230 <55931023+lhj1230@users.noreply.github.com>
2025-05-29 13:03:29 +0900

    Update start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
-cd /home/ubuntu/scripts
-docker compose up -d --build
+docker stop spring-petclinic || true
+docker rm spring-petclinic || true
+
+docker run -d -p 80:8080 \
+  -v /home/ubuntu/target:/app \
+  --name spring-petclinic \
+  openjdk:21 \
+  java -jar /app/spring-petclinic-*.jar
